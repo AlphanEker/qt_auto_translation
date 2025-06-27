707396bc43b8f0366d6b6c69fa9bdf23a05c7aaf
PoePew <alphaneker40@gmail.com>
2025-06-27 09:58:37 +0300

    example ts file

---
+++ language_tr_TR.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr_TR">
+<context>
+    <name>AlarmPage</name>
+    <message>
+        <location filename="../../src/gui/page/AlarmPage.ui" line="14"/>
+        <location filename="../../build/Desktop_Qt_6_7_3_MSVC2022_64bit-Debug/UI_autogen/include/ui_AlarmPage.h" line="130"/>
+        <location filename="../../build/Desktop_Qt_6_7_3_MSVC2022_64bit-Release/UI_autogen/include/ui_AlarmPage.h" line="130"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/page/AlarmPage.ui" line="157"/>
+        <location filename="../../build/Desktop_Qt_6_7_3_MSVC2022_64bit-Debug/UI_autogen/include/ui_AlarmPage.h" line="133"/>
+        <location filename="../../build/Desktop_Qt_6_7_3_MSVC2022_64bit-Release/UI_autogen/include/ui_AlarmPage.h" line="133"/>
+        <source>text</source>
+        <translation>metin</translation>
+    </message>
+    <message>
+        <location filename="../../src/gui/page/AlarmPage.cpp" line="14"/>
+        <source>Alarm Logs</source>
+        <translation>Alarm Sayfası</translation>
+    </message>
+</context>
+</TS>
